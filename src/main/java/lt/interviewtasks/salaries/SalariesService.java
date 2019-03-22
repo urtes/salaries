2078a3331d17fd012d76e8381985fdda9cade6fb
Urte <urte.rimsaite@gmail.com>
2019-03-22 15:11:43 +0200

    Edit writeToCsv

--- src/main/java/lt/interviewtasks/salaries/SalariesService.java
+++ src/main/java/lt/interviewtasks/salaries/SalariesService.java
@@ -14,6 +14,7 @@ public class SalariesService {
     private Map<String, Long> paymentsSumByType = new HashMap();
 
     private final Float TAX_FACTOR = 0.4F;
+    private final String ERROR_MESSAGE = "Invalid report type";
     public final static String OUTPUT_DELIMITER = ";";
     private final String INPUT_DELIMITER = ",";
     private final String PATH = "src/main/resources/static/";
@@ -22,12 +23,12 @@ public class SalariesService {
     private final String OUTPUT_FILE_SUM_TYPE = "suma_tipas.csv";
 
     private final String SUM_TAXES_HEADER = "Darbuotojas" + OUTPUT_DELIMITER
-                                            + "Suma" + OUTPUT_DELIMITER
-                                            + "Mokesciai";
+            + "Suma" + OUTPUT_DELIMITER
+            + "Mokesciai";
 
     private final String SUM_TYPE_HEADER = "Darbuotojas" + OUTPUT_DELIMITER
-                                            + "Tipas" + OUTPUT_DELIMITER
-                                            + "Suma";
+            + "Tipas" + OUTPUT_DELIMITER
+            + "Suma";
 
     private enum ReportType {
         SUM_TAXES, SUM_TYPE
@@ -39,14 +40,14 @@ public class SalariesService {
         processData();
 
         writeDataToCsv(paymentsSumAndTaxes,
-                        SUM_TAXES_HEADER,
-                        OUTPUT_FILE_SUM_TAXES,
-                        ReportType.SUM_TAXES);
+                SUM_TAXES_HEADER,
+                OUTPUT_FILE_SUM_TAXES,
+                ReportType.SUM_TAXES);
 
         writeDataToCsv(paymentsSumByType,
-                        SUM_TYPE_HEADER,
-                        OUTPUT_FILE_SUM_TYPE,
-                        ReportType.SUM_TYPE);
+                SUM_TYPE_HEADER,
+                OUTPUT_FILE_SUM_TYPE,
+                ReportType.SUM_TYPE);
     }
 
     private void loadDataFromCsv() {
@@ -82,41 +83,7 @@ public class SalariesService {
                         Collectors.summingLong(Payment::getPaymentAmount)));
     }
 
-//    private void writeDataToCsv(Map<String, Long> data,
-//                                String header,
-//                                String filename,
-//                                ReportType reportType) {
-//
-//        Map<String, Long> sortedData = new TreeMap(data);
-//        File file = new File(PATH + filename);
-//        file.getParentFile().mkdirs();
-//        PrintWriter printWriter = null;
-//        String outputLine = "";
-//
-//        try {
-//            printWriter = new PrintWriter(file);
-//            printWriter.println(header);
-//            for(Map.Entry<String, Long> entry : sortedData.entrySet()) {
-//
-//                if (reportType == ReportType.SUM_TAXES) {
-//                    outputLine = entry.getKey() + OUTPUT_DELIMITER
-//                            + entry.getValue() + OUTPUT_DELIMITER
-//                            + entry.getValue() * TAX_FACTOR;
-//                } else if (reportType == ReportType.SUM_TYPE) {
-//                    outputLine = entry.getKey() + OUTPUT_DELIMITER + entry.getValue();
-//                }
-//
-//                printWriter.println(outputLine);
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (printWriter != null) {
-//                printWriter.close();
-//            }
-//        }
-//    }
-    private void writeDataToCsv(Map<String, Long> data,
+        private void writeDataToCsv(Map<String, Long> data,
                                 String header,
                                 String filename,
                                 ReportType reportType) {
@@ -125,21 +92,25 @@ public class SalariesService {
         File file = new File(PATH + filename);
         file.getParentFile().mkdirs();
         PrintWriter printWriter = null;
+        String outputLine = "";
 
         try {
             printWriter = new PrintWriter(file);
             printWriter.println(header);
+            for(Map.Entry<String, Long> entry : sortedData.entrySet()) {
+
                 if (reportType == ReportType.SUM_TAXES) {
-                    for(Map.Entry<String, Long> entry : sortedData.entrySet()) {
-                        printWriter.println(entry.getKey() + OUTPUT_DELIMITER
-                                            + entry.getValue() + OUTPUT_DELIMITER
-                                            + entry.getValue() * TAX_FACTOR);
-                    }
+                    outputLine = entry.getKey() + OUTPUT_DELIMITER
+                            + entry.getValue() + OUTPUT_DELIMITER
+                            + entry.getValue() * TAX_FACTOR;
                 } else if (reportType == ReportType.SUM_TYPE) {
-                    for(Map.Entry<String, Long> entry : sortedData.entrySet()) {
-                        printWriter.println(entry.getKey() + OUTPUT_DELIMITER + entry.getValue());
-                    }
+                    outputLine = entry.getKey() + OUTPUT_DELIMITER + entry.getValue();
+                } else {
+                    System.out.println(ERROR_MESSAGE);
                 }
+
+                printWriter.println(outputLine);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
